d63309dbeef19ed8a32133acf78077c88be0e24b
HatsuSixty <hatsunotsus@gmail.com>
2025-06-09 12:32:59 -0300

    [screenshot_linux] Abstract running commands into a function

--- src/screenshot_linux.c
+++ src/screenshot_linux.c
@@ -1,71 +1,55 @@
 #include "screenshot.h"
 
 #include <errno.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-static bool take_screenshot_wayland(const char* screenshot_file_path)
+static bool run_command(char* exe, ...)
 {
-    pid_t grim = fork();
-    if (grim == 0) {
-        if (execlp("grim", "grim", screenshot_file_path, NULL) == -1) {
-            fprintf(stderr, "ERROR: could not execute `grim`: %s\n",
-                    strerror(errno));
+    char* args_array[256] = { exe };
+    int args_array_count = 1;
+
+    va_list args;
+    va_start(args, exe);
+
+    while (true) {
+        char* arg = va_arg(args, char*);
+        if (!arg)
+            break;
+        args_array[args_array_count++] = arg;
+    }
+    args_array[args_array_count++] = NULL;
+
+    va_end(args);
+
+    pid_t process = fork();
+    if (process == 0) {
+        if (execvp(exe, args_array) == -1) {
+            fprintf(stderr, "ERROR: could not execute `%s`: %s\n",
+                    exe, strerror(errno));
             exit(1);
         }
         exit(0);
     }
 
-    int grim_status = 0;
-    if (waitpid(grim, &grim_status, 0) == -1) {
+    int process_status = 0;
+    if (waitpid(process, &process_status, 0) == -1) {
         fprintf(stderr, "ERROR: could not `waitpid`: %s\n", strerror(errno));
         return false;
     }
 
-    if (!WIFEXITED(grim_status)) {
-        fprintf(stderr, "ERROR: `grim` subprocess exited abnormally\n");
+    if (!WIFEXITED(process_status)) {
+        fprintf(stderr, "ERROR: `%s` subprocess exited abnormally\n", exe);
         return false;
     }
 
-    int grim_return_code = WEXITSTATUS(grim_status);
-    if (grim_return_code != 0) {
-        fprintf(stderr, "ERROR: `grim` subprocess exited with code `%d`\n",
-                grim_return_code);
-        return false;
-    }
-
-    return true;
-}
-
-static bool take_screenshot_x11(const char* screenshot_file_path)
-{
-    pid_t scrot = fork();
-    if (scrot == 0) {
-        if (execlp("scrot", "scrot", "-o", "-F", screenshot_file_path, NULL) == -1) {
-            fprintf(stderr, "ERROR: could not execute `scrot`: %s\n",
-                    strerror(errno));
-            exit(1);
-        }
-        exit(0);
-    }
-
-    int scrot_status = 0;
-    if (waitpid(scrot, &scrot_status, 0) == -1) {
-        fprintf(stderr, "ERROR: could not `waitpid`: %s\n", strerror(errno));
-        return false;
-    }
-
-    if (!WIFEXITED(scrot_status)) {
-        fprintf(stderr, "ERROR: `scrot` subprocess exited abnormally\n");
-        return false;
-    }
-
-    int scrot_return_code = WEXITSTATUS(scrot_status);
-    if (scrot_return_code != 0) {
-        fprintf(stderr, "ERROR: `scrot` subprocess exited with code `%d`\n",
-                scrot_return_code);
+    int process_return_code = WEXITSTATUS(process_status);
+    if (process_return_code != 0) {
+        fprintf(stderr, "ERROR: `%s` subprocess exited with code `%d`\n",
+                exe, process_return_code);
         return false;
     }
 
@@ -74,35 +58,17 @@ static bool take_screenshot_x11(const char* screenshot_file_path)
 
 static bool take_screenshot_spectacle(const char* screenshot_file_path)
 {
-    pid_t spectacle = fork();
-    if (spectacle == 0) {
-        if (execlp("spectacle", "spectacle", "-b", "-n", "-o", screenshot_file_path, NULL) == -1) {
-            fprintf(stderr, "ERROR: could not execute `spectacle`: %s\n",
-                    strerror(errno));
-            exit(1);
-        }
-        exit(0);
-    }
+    return run_command("spectacle", "-b", "-n", "-o", screenshot_file_path, NULL);
+}
 
-    int spectacle_status = 0;
-    if (waitpid(spectacle, &spectacle_status, 0) == -1) {
-        fprintf(stderr, "ERROR: could not `waitpid`: %s\n", strerror(errno));
-        return false;
-    }
+static bool take_screenshot_grim(const char* screenshot_file_path)
+{
+    return run_command("grim", screenshot_file_path, NULL);
+}
 
-    if (!WIFEXITED(spectacle_status)) {
-        fprintf(stderr, "ERROR: `spectacle` subprocess exited abnormally\n");
-        return false;
-    }
-
-    int spectacle_return_code = WEXITSTATUS(spectacle_status);
-    if (spectacle_return_code != 0) {
-        fprintf(stderr, "ERROR: `spectacle` subprocess exited with code `%d`\n",
-                spectacle_return_code);
-        return false;
-    }
-
-    return true;
+static bool take_screenshot_x11(const char* screenshot_file_path)
+{
+    return run_command("scrot", "-o", "-F", screenshot_file_path, NULL);
 }
 
 bool take_screenshot(const char* screenshot_file_path)
@@ -112,10 +78,10 @@ bool take_screenshot(const char* screenshot_file_path)
 
     char const* xdg_current_desktop = getenv("XDG_CURRENT_DESKTOP");
     if (xdg_current_desktop == NULL)
-        return take_screenshot_wayland(screenshot_file_path);
+        return take_screenshot_grim(screenshot_file_path);
 
     if (strcmp(xdg_current_desktop, "KDE") == 0)
         return take_screenshot_spectacle(screenshot_file_path);
 
-    return take_screenshot_wayland(screenshot_file_path);
+    return take_screenshot_grim(screenshot_file_path);
 }
